eab4d98d71d87e43038c876939d781b0aa1e01d4
RenaissanceduPhoenix <RenaissanceduPhoenix@proton.me>
2026-04-02 15:40:04 +0200

    Update Firebase.js

--- Firebase.js
+++ Firebase.js
@@ -1,7 +1,7 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
-firebase
+FIREBASE
 // Initialisation
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app); // Le mot-clé 'export' est crucial ici
